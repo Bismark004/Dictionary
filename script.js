f743750d9b4520776a336819cb05cca9c8bb80c4
Bismark Adzai <116949093+Bismark004@users.noreply.github.com>
2023-06-06 01:31:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const search_Btn = document.getElementById("search-btn");
+const search_Box = document.querySelector(".search-box");
+const resultContainer = document.querySelector(".result");
+
+const renderError = (response) => {
+  if (response.title) throw new Error(`${response.title}`);
+};
+
+function renderWord(data, word) {
+  const details = data.meanings[0].partOfSpeech;
+  const meaning = data.meanings[0].definitions[0].definition;
+  const example = data.meanings[0].definitions[0].example || "";
+
+  const html = `
+<div class="word"> <h3>${word}</h3> </div>
+    <div class="details"><p>${details}</p></div>
+    <p class="word-meaning">${meaning}</p>
+    <p class="word-example">${example}</p>`;
+
+  resultContainer.innerHTML = html;
+}
+
+const loadResult = async () => {
+  try {
+    const inputWord = document.getElementById("inp-word").value;
+    const word = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`
+    );
+    const wordData = await word.json();
+    renderError(wordData);
+
+    renderWord(wordData[0], inputWord);
+  } catch (error) {
+    resultContainer.innerHTML = `<h3 class="error"> ${error.message}</h3>`;
+  }
+};
+
+search_Btn.addEventListener("click", loadResult);
+
+search_Box.addEventListener("keydown", function (e) {
+  return e.key === "Enter" ? loadResult() : null;
+});
+
+let darkMode = false;
+function ToDarkMode() {
+  darkMode = !darkMode;
+  applyTheme();
+}
+function applyTheme() {
+  let body = document.getElementsByTagName("body")[0];
+  if (darkMode) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+}
